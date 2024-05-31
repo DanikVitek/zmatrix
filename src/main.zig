16f89added819596b2192f1dc18bc45b77568230
Danik Vitek <x3665107@gmail.com>
2024-06-01 01:08:47 +0300

    Implement the basics

---
+++ src/main.zig
@@ -0,0 +1,236 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const @"ansi-term" = @import("ansi-term");
+const clear = @"ansi-term".clear;
+const cursor = @"ansi-term".cursor;
+const format = @"ansi-term".format;
+const style = @"ansi-term".style;
+
+const BufWriter = @TypeOf(std.io.bufferedWriter(std.io.getStdOut().writer()));
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const alloc = arena.allocator();
+
+    var buf_stdout = std.io.bufferedWriter(std.io.getStdOut().writer());
+
+    var prng = std.rand.DefaultPrng.init(0);
+    const rng = prng.random();
+
+    try cursor.hideCursor(buf_stdout.writer());
+
+    var rain = try Rain.init(
+        alloc,
+        rng,
+        .{ .r = 0, .g = 255, .b = 0 },
+        .{ .at_least = 3, .at_most = 10 },
+        .{ .at_least = 1, .at_most = 3 },
+        15,
+        100 * std.time.ns_per_ms,
+    );
+    try rain.draw(&buf_stdout);
+
+    try clear.clearScreen(buf_stdout.writer());
+    try format.resetStyle(buf_stdout.writer());
+    try cursor.showCursor(buf_stdout.writer());
+    try buf_stdout.flush();
+}
+
+fn Range(comptime T: type) type {
+    return struct {
+        at_least: T,
+        at_most: T,
+
+        const Self = @This();
+
+        pub fn genInt(self: *const Self, rng: std.Random) T {
+            if (@typeInfo(T) != .Int) {
+                @compileError("requires integer type for Range.genInt()");
+            }
+            return rng.intRangeAtMost(T, self.at_least, self.at_most);
+        }
+    };
+}
+
+const Rain = struct {
+    color: style.ColorRGB,
+    drop_len_range: Range(u8),
+    drop_speed_range: Range(u16),
+    drops_amount: u16,
+    drops: std.ArrayList(Raindrop),
+    rng: std.Random,
+    frame_delay_ns: u64,
+
+    fn init(
+        alloc: std.mem.Allocator,
+        rng: std.Random,
+        color: style.ColorRGB,
+        drop_len_range: Range(u8),
+        drop_speed_range: Range(u16),
+        drops_amount: u16,
+        frame_delay_ns: u64,
+    ) !Rain {
+        var raindrops = try std.ArrayList(Raindrop).initCapacity(alloc, drops_amount);
+        for (0..drops_amount) |_| {
+            const len = drop_len_range.genInt(rng);
+            raindrops.appendAssumeCapacity(Raindrop{
+                .len = len,
+                .x = rng.intRangeLessThan(u16, 0, try termWidth()),
+                .y = 0,
+                .color = color,
+                .speed = drop_speed_range.genInt(rng),
+            });
+        }
+        return Rain{
+            .drops = raindrops,
+            .rng = rng,
+            .color = color,
+            .drop_len_range = drop_len_range,
+            .drop_speed_range = drop_speed_range,
+            .drops_amount = drops_amount,
+            .frame_delay_ns = frame_delay_ns,
+        };
+    }
+
+    fn draw(self: *Rain, buf_writer: *BufWriter) !void {
+        const writer = buf_writer.writer();
+
+        try clear.clearScreen(writer);
+        try cursor.setCursor(writer, 0, 0);
+
+        while (true) {
+            for (0..self.drops.items.len) |i| {
+                const drop = &self.drops.items[i];
+
+                try drop.draw(writer);
+                drop.fall();
+                try drop.clearTail(writer);
+
+                if (try drop.isPastEnd()) {
+                    _ = self.drops.swapRemove(i);
+                    try self.addNewDrop();
+                }
+            }
+            try buf_writer.flush();
+            std.time.sleep(self.frame_delay_ns);
+        }
+    }
+
+    fn addNewDrop(self: *Rain) !void {
+        try self.drops.append(Raindrop{
+            .len = self.drop_len_range.genInt(self.rng),
+            .x = self.rng.intRangeLessThan(u16, 0, try termWidth()),
+            .y = 0,
+            .color = self.color,
+            .speed = self.drop_speed_range.genInt(self.rng),
+        });
+    }
+};
+
+const Raindrop = struct {
+    x: u16,
+    /// the head (bottom) of the raindrop. 0 means the the droplet is out of the screen from the top.
+    y: u16,
+    speed: u16,
+    len: u8,
+    color: style.ColorRGB,
+
+    fn draw(self: *const Raindrop, writer: anytype) !void {
+        var r: f32 = 0;
+        var g: f32 = 0;
+        var b: f32 = 0;
+
+        const step_r: f32 = @as(f32, @floatFromInt(self.color.r)) / @as(f32, @floatFromInt(self.len));
+        const step_g: f32 = @as(f32, @floatFromInt(self.color.g)) / @as(f32, @floatFromInt(self.len));
+        const step_b: f32 = @as(f32, @floatFromInt(self.color.b)) / @as(f32, @floatFromInt(self.len));
+
+        var prev_color: ?style.ColorRGB = null;
+        for (0..self.len) |_i| {
+            const i: u8 = @intCast(_i);
+            defer {
+                r += step_r;
+                g += step_g;
+                b += step_b;
+            }
+            // std.debug.print("[{*}]  y: {d}; i: {d}; len: {d}; speed: {d}\n", .{ self, self.y, i, self.len, self.speed });
+
+            // y_i < 1 or y_i > termHeight() -> out of screen
+            if (self.y + i < self.len or self.y + i + 1 - self.len > try termHeight()) continue; // skip if out of screen
+            const y_i = self.y + i + 1 - self.len;
+
+            // std.debug.print("y_i: {d}\n", .{y_i});
+
+            const color: style.ColorRGB = .{
+                .r = @intFromFloat(r),
+                .g = @intFromFloat(g),
+                .b = @intFromFloat(b),
+            };
+
+            try cursor.setCursor(writer, self.x, y_i);
+            try updateColor(writer, color, prev_color);
+            try writer.print("{u}", .{self.grapheme(y_i)});
+
+            prev_color = color;
+        }
+    }
+
+    fn fall(self: *Raindrop) void {
+        self.y += self.speed;
+    }
+
+    fn clearTail(self: *const Raindrop, writer: anytype) !void {
+        for (0..self.len) |_i| {
+            const i: u16 = @intCast(_i);
+            try cursor.setCursor(writer, self.x, self.y -| (self.len + i));
+            try writer.writeByte(' ');
+        }
+    }
+
+    fn grapheme(self: *const Raindrop, pos: u16) u21 {
+        var hasher = std.hash.Adler32.init();
+        std.hash.autoHash(&hasher, .{ self, pos });
+
+        return @intCast((hasher.final() % 93) + 33); // TODO: use unicode graphemes
+    }
+
+    fn isPastEnd(self: *const Raindrop) !bool {
+        return self.y + 2 > self.len + try termHeight();
+    }
+};
+
+fn updateColor(writer: anytype, new: style.ColorRGB, old: ?style.ColorRGB) !void {
+    try format.updateStyle(
+        writer,
+        .{ .foreground = .{ .RGB = new } },
+        if (old) |o| .{ .foreground = .{ .RGB = o } } else null,
+    );
+}
+
+inline fn termWidth() !u16 {
+    return switch (builtin.os.tag) {
+        .windows => @import("windows.zig").termWidth(),
+        else => @compileError("Unsupported OS"),
+    };
+}
+
+inline fn termHeight() !u16 {
+    return switch (builtin.os.tag) {
+        .windows => @import("windows.zig").termHeight(),
+        else => @compileError("Unsupported OS"),
+    };
+}
+
+inline fn enableRawMode() !void {
+    switch (builtin.os.tag) {
+        .windows => @import("windows.zig").enableRawMode(),
+        else => @compileError("Unsupported OS"),
+    }
+}
+
+inline fn disableRawMode() !void {
+    switch (builtin.os.tag) {
+        .windows => @import("windows.zig").disableRawMode(),
+        else => @compileError("Unsupported OS"),
+    }
+}
---
+++ src/windows.zig
@@ -0,0 +1,203 @@
+const std = @import("std");
+const win = std.os.windows;
+
+pub fn termWidth() !u16 {
+    const srWindow = (try (try ScreenBuffer.current()).info()).srWindow;
+    return @intCast(srWindow.Right - srWindow.Left + 1);
+}
+
+pub fn termHeight() !u16 {
+    const srWindow = (try (try ScreenBuffer.current()).info()).srWindow;
+    return @intCast(srWindow.Bottom - srWindow.Top + 1);
+}
+
+const ScreenBuffer = struct {
+    handle: Handle,
+
+    fn current() !ScreenBuffer {
+        return .{ .handle = try Handle.init(.current_output_handle) };
+    }
+
+    fn info(self: ScreenBuffer) !win.CONSOLE_SCREEN_BUFFER_INFO {
+        var csbi = std.mem.zeroes(win.CONSOLE_SCREEN_BUFFER_INFO);
+        try result(win.kernel32.GetConsoleScreenBufferInfo(self.handle.inner, &csbi));
+        return csbi;
+    }
+};
+
+pub fn enableRawMode() !void {
+    const console_mode = ConsoleMode{ .handle = try Handle.currentInHandle() };
+
+    const dw_mode = try console_mode.mode();
+
+    const new_mode = dw_mode & ~NOT_RAW_MODE_MASK;
+
+    try console_mode.setMode(new_mode);
+}
+
+pub fn disableRawMode() !void {
+    const console_mode = ConsoleMode{ .handle = try Handle.currentInHandle() };
+
+    const dw_mode = try console_mode.mode();
+
+    const new_mode = dw_mode | NOT_RAW_MODE_MASK;
+
+    try console_mode.setMode(new_mode);
+}
+
+const Handle = struct {
+    inner: win.HANDLE,
+
+    pub inline fn init(comptime handle_type: HandleType) !Handle {
+        return switch (handle_type) {
+            .output_handle => try outputHandle(),
+            .input_handle => try inputHandle(),
+            .current_output_handle => try currentOutHandle(),
+            .current_input_handle => try currentInHandle(),
+        };
+    }
+
+    pub fn currentOutHandle() !Handle {
+        return .{ .inner = try handleResult(win.kernel32.CreateFileW(
+            comptime encodeUtf16Z("CONOUT$"),
+            win.GENERIC_READ | win.GENERIC_WRITE,
+            win.FILE_SHARE_READ | win.FILE_SHARE_WRITE,
+            null,
+            win.OPEN_EXISTING,
+            0,
+            null,
+        )) };
+    }
+
+    pub fn currentInHandle() !Handle {
+        return .{ .inner = try handleResult(win.kernel32.CreateFileW(
+            comptime encodeUtf16Z("CONIN$"),
+            win.GENERIC_READ | win.GENERIC_WRITE,
+            win.FILE_SHARE_READ | win.FILE_SHARE_WRITE,
+            null,
+            win.OPEN_EXISTING,
+            0,
+            null,
+        )) };
+    }
+
+    pub fn outputHandle() !Handle {
+        const STD_OUTPUT_HANDLE: win.DWORD = 0xFFFF_FFF5;
+        return .{ .inner = try handleResult(win.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)) };
+    }
+
+    pub fn inputHandle() !Handle {
+        const STD_INPUT_HANDLE: win.DWORD = 0xFFFF_FFF6;
+        return .{ .inner = try handleResult(win.kernel32.GetStdHandle(STD_INPUT_HANDLE)) };
+    }
+
+    fn stdHandle(which_std: win.DWORD) !Handle {
+        return .{ .inner = try handleResult(win.kernel32.GetStdHandle(which_std)) };
+    }
+};
+
+const HandleType = enum {
+    /// The process' standard output handle.
+    output_handle,
+    /// The process' standard input handle.
+    input_handle,
+    /// The process' active console screen buffer, `CONOUT$`.
+    current_output_handle,
+    /// The process' console input buffer, `CONIN$`.
+    current_input_handle,
+};
+
+const ConsoleMode = struct {
+    handle: Handle,
+
+    fn mode(self: ConsoleMode) !u32 {
+        var console_mode: win.DWORD = 0;
+        if (win.kernel32.GetConsoleMode(self.handle.inner, &console_mode) != 0) {
+            return @errorFromInt(@intFromEnum(win.kernel32.GetLastError()));
+        }
+        return console_mode;
+    }
+
+    fn setMode(self: ConsoleMode, dw_mode: u32) !void {
+        return result(SetConsoleMode(self.handle.inner, dw_mode));
+    }
+};
+
+pub extern "kernel32" fn SetConsoleMode(in_hConsoleHandle: win.HANDLE, in_dwMode: win.DWORD) callconv(win.WINAPI) win.BOOL;
+
+const NOT_RAW_MODE_MASK: u32 = b: {
+    const ENABLE_PROCESSED_INPUT = 0x0001;
+    const ENABLE_LINE_INPUT = 0x0002;
+    const ENABLE_ECHO_INPUT = 0x0004;
+    break :b ENABLE_PROCESSED_INPUT | ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT;
+};
+
+inline fn handleResult(return_value: win.HANDLE) Win32Error!win.HANDLE {
+    return if (return_value != win.INVALID_HANDLE_VALUE)
+        return_value
+    else
+        @as(Win32Error, @errorCast(@errorFromInt(@intFromEnum(win.kernel32.GetLastError()))));
+}
+
+inline fn result(return_value: win.BOOL) Win32Error!void {
+    if (return_value == 0) {
+        return @as(Win32Error, @errorCast(@errorFromInt(@intFromEnum(win.kernel32.GetLastError()))));
+    }
+}
+
+const Win32Error = b: {
+    var error_set: []const std.builtin.Type.Error = &.{};
+    @setEvalBranchQuota(@typeInfo(win.Win32Error).Enum.fields.len);
+    for (@typeInfo(win.Win32Error).Enum.fields) |field| {
+        error_set = error_set ++ .{.{ .name = field.name }};
+    }
+    break :b @Type(.{ .ErrorSet = error_set });
+};
+
+fn encodeUtf16Z(comptime utf8: []const u8) *const [countCodepoints(utf8):0]u16 {
+    comptime {
+        var utf8_iter = std.unicode.Utf8View.initComptime(utf8).iterator();
+
+        var utf16: []const u16 = &.{};
+        var extra: u16 = 0;
+
+        while (true) {
+            if (extra != 0) {
+                const tmp = extra;
+                extra = 0;
+                utf16 = utf16 ++ .{tmp};
+                continue;
+            }
+
+            var buf: [2]u16 = .{0} ** 2;
+            if (utf8_iter.nextCodepoint()) |ch| {
+                const n = if ((ch & 0xFFFF) == ch) b: {
+                    buf[0] = @intCast(ch);
+                    break :b 1;
+                } else b: {
+                    const code = ch - 0x1_0000;
+                    buf[0] = 0xD800 | @as(u16, @intCast(code >> 10));
+                    buf[1] = 0xDC00 | (@as(u16, @intCast(code)) & 0x3FF);
+                    break :b 2;
+                };
+                if (n == 2) {
+                    extra = buf[1];
+                }
+                utf16 = utf16 ++ .{buf[0]};
+            } else break;
+        }
+
+        utf16 = utf16 ++ .{0};
+        return utf16[0 .. utf16.len - 1 :0];
+    }
+}
+
+fn countCodepoints(comptime utf8: []const u8) comptime_int {
+    var utf8_iter = std.unicode.Utf8View.initComptime(utf8).iterator();
+
+    var count = 0;
+    while (utf8_iter.nextCodepoint()) |_| {
+        count += 1;
+    }
+    return count;
+}
